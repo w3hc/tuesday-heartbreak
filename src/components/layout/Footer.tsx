a02a92aa2947e55209fe8ebe24fb724ea6274ab7
julienbrg <julien@strat.cc>
2023-10-01 16:25:33 +0200

    rm footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -13,7 +13,7 @@ export function Footer(props: Props) {
 
   return (
     <Flex as="footer" className={className} flexDirection="column" justifyContent="center" alignItems="center" my={8}>
-      <Text>{SITE_DESCRIPTION}</Text>
+      {/* <Text>{SITE_DESCRIPTION}</Text>
 
       <Flex color="gray.500" gap={2} alignItems="center" mt={2}>
         <LinkComponent href={`https://github.com/${SOCIAL_GITHUB}`}>
@@ -22,7 +22,7 @@ export function Footer(props: Props) {
         <LinkComponent href={`https://twitter.com/${SOCIAL_TWITTER}`}>
           <FaTwitter />
         </LinkComponent>
-      </Flex>
+      </Flex> */}
     </Flex>
   )
 }
